86a14772f4073c42c15740bfce5f9512ba90ec8e
Vijay Singh <vijaysingh997022@gmail.com>
2023-04-20 15:55:37 +0530

    Addes static files and templates

--- store/models.py
+++ store/models.py
@@ -8,14 +8,14 @@ class Category(models.Model):
     name = models.CharField(max_length=250, db_index=True)
     slug = models.SlugField(max_length=250, unique=True)
 
-    class Meta:
-        verbose_name_plural = 'categories'
-
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('list-category', args=[self.slug])
+
+    class Meta:
+        verbose_name_plural = 'categories'
 
 
 class Product(models.Model):
@@ -28,11 +28,11 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=4, decimal_places=2)
     image = models.ImageField(upload_to='images/')
 
-    class Meta:
-        verbose_name_plural = 'products'
-
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('product-info', args=[self.slug])
+
+    class Meta:
+        verbose_name_plural = 'products'
