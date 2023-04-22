43df8c49d0f82264eacd1d6e6905a3b5d99eb60a
Vijay Singh <vijaysingh997022@gmail.com>
2023-04-22 16:43:44 +0530

    Store

--- store/admin.py
+++ store/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 
-from .models import Category, Product
-
 # Register your models here.
+
+from . models import Category, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+
     prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+
     prepopulated_fields = {'slug': ('title',)}
+
+
